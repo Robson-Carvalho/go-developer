0b2e614865f2bfe25e7b990a785fd5d9dd2e6783
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2024-01-13 22:17:36 -0300

    feat: estudos de erro

--- errors-panic-recover/main.go
+++ errors-panic-recover/main.go
@@ -3,34 +3,35 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main(){
-    /*res, err := http.Get("http://google.com.br")
+    res, err := http.Get("http://google.com.br")
     if err != nil {
         log.Fatal(err.Error())
     }
 
     fmt.Println(res.Body)
-    */
 
-    /*
-    soma, err := soma(1, 29)
+
+
+    soma, err := somaF(1, 29)
     if err != nil{
         log.Fatal(err.Error())
     }
 
     fmt.Printf("o valor da soma é: %v\n", soma)
 
-    */
 
     // Uma forma de não tratar o erro
-    soma2, _ := soma(2,30)
+    soma2, _ := somaF(2,30)
 
     fmt.Printf("o valor da soma é: %v\n", soma2)
 }
 
-func soma(x int, y int) (int, error){
+func somaF(x int, y int) (int, error){
     res := x + y
 
     if res > 10 {
